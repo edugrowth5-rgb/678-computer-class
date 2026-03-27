2c0bb1a568744f62de1008119314fb2ecfeacaed
edugrowth5-rgb <edugrowth5@gmail.com>
2026-03-27 19:35:41 +0530

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,29 @@
+const cacheName = 'sit-learning-v1';
+const assets = [
+  'S678index.html',
+  'S678style.css',
+  'S678engine.js',
+  'Sdata6.js',
+  'Sdata7.js',
+  'Sdata8.js',
+  'manifest.json'
+];
+
+// Install Service Worker
+self.addEventListener('install', evt => {
+  evt.waitUntil(
+    caches.open(cacheName).then(cache => {
+      console.log('Caching all assets');
+      cache.addAll(assets);
+    })
+  );
+});
+
+// Fetch Assets
+self.addEventListener('fetch', evt => {
+  evt.respondWith(
+    caches.match(evt.request).then(cacheRes => {
+      return cacheRes || fetch(evt.request);
+    })
+  );
+});
